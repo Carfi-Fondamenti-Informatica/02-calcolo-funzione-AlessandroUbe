5dcb9430fa181e796dfa351dd3b3d4a316b043b2
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-11-14 14:45:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    float a=0;
+    float b=0;
+    float x=0;
+    float y=0;
+
+    cin>>a; cin>>b; cin>>x; cin>>y;
+
+    if(x<0 && y>0)
+        cout<<"z= "<< a*x-b*y;
+    else if(x>=0 && y<=0)
+        cout<<"z= "<< a*x*x-b*y;
+    else
+        cout<<"z="<< a*x+b*y*y;
    return 0;
 }
